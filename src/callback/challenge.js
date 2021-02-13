8ca4509f25aa1ec5448a639136ef81c63804467e
Sergio Piña <alonzopina7@gmail.com>
2021-02-13 16:06:54 -0600

    first

---
+++ src/callback/challenge.js
@@ -0,0 +1,19 @@
+let XMLHttpsRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function fetchData(url_api, callback) {
+  let xhttp = new XMLHttpsRequest();
+  xhttp.open("GET", url_api, true);
+  xhttp.onreadystatechange = function (event) {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error("Error" + url_api);
+        return callback(error, null);
+      }
+    }
+  };
+  xhttp.send();
+}
+
+
